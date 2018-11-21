dadfd56a92392d1d1e7f294afff0142b74a82f4b
ChasteSui <804959070@qq.com>
2018-11-21 15:48:37 +0800

    update img
    God bless jun

--- Love_files/functions.js
+++ Love_files/functions.js
@@ -38,23 +38,23 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var current = new Date();
+	var seconds = (current.getTime() - date.getTime()) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
+	var secondsTemp1 = seconds % (3600 * 24);
+	var hours = Math.floor(secondsTemp1 / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
+	var secondsTemp2 = secondsTemp1 % 3600;
+	var minutes = Math.floor(secondsTemp2 / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
+	var secondsTemp3 = Math.round(secondsTemp2 % 60);
+	if (secondsTemp3 < 10) {
+		secondsTemp3 = "0" + secondsTemp3;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + secondsTemp3 + "</span> 秒"; 
 	$("#clock").html(result);
 }
